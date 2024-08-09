58eefe6978a334979ad83fd524871d22401d77c4
Aditya kumar <107586594+Aditya-21052131@users.noreply.github.com>
2024-08-09 15:44:51 +0530

    Create Register.js

---
+++ public/src/components/Auth/Register.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+function Register() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [email, setEmail] = useState('');
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        axios.post('/api/users/', { username, password, email })
+            .then(response => {
+                console.log(response.data);
+            })
+            .catch(error => {
+                console.error(error);
+            });
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <label>
+                Username:
+                <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+            </label>
+            <label>
+                Password:
+                <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+            </label>
+            <label>
+                Email:
+                <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+            </label>
+            <button type="submit">Register</button>
+        </form>
+    );
+}
+
+export default Register;
