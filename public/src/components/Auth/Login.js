ed9bc88e87072e46a79a0ef6dbfaf4bb9ed331f3
Aditya kumar <107586594+Aditya-21052131@users.noreply.github.com>
2024-08-09 15:45:23 +0530

    Create Login.js

---
+++ public/src/components/Auth/Login.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+function Login() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        axios.post('/api-token-auth/', { username, password })
+            .then(response => {
+                console.log(response.data);
+            })
+            .catch(error => {
+                console.error(error);
+            });
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <label>
+                Username:
+                <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+            </label>
+            <label>
+                Password:
+                <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+            </label>
+            <button type="submit">Login</button>
+        </form>
+    );
+}
+
+export default Login;
